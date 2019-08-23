9832e1f24bf9216430a5ce6a14ec6baa244bf421
Sanchez <smizinec@gmail.com>
2019-08-23 20:26:01 +0300

    modify reducer no work

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
-import updateBookList from './BookList';
-import updateShoppingCart from './ShoppingCart';
+// import updateBookList from './BookList';
+// import updateShoppingCart from './ShoppingCart';
 
-const reducer = (state, action) => {
-    // console.log(action.type);
-        return {
-            bookList: updateBookList(state, action),
-            shoppingCart: updateShoppingCart(state, action),
-        }
-}
+// const reducer = (state, action) => {
+//     // console.log(action.type);
+//         return {
+//             bookList: updateBookList(state, action),
+//             shoppingCart: updateShoppingCart(state, action),
+//         }
+// }
 
-export default reducer;
+// export default reducer;
